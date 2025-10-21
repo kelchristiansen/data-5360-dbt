9e3941b417161e548a68e72927732a0803326165
Chelsea Christiansen <chelsea.christiansen@usu.edu>
2025-10-21 18:23:08 +0000

    dbt2 first half

--- models/insurance/insurance_dim_date.sql
+++ models/insurance/insurance_dim_date.sql
@@ -1,18 +1,18 @@
 {{ config(
     materialized = 'table',
-    schema = 'dw_oliver'
-    )
-}}
+    schema = 'dw_insurance'
+)}}
 
 with cte_date as (
 {{ dbt_date.get_date_dimension("1990-01-01", "2050-12-31") }}
 )
 
 SELECT
-date_id as date_key,
-date_id,
-dayofweek,
-month,
-quarter,
-year
+date_day as date_key,
+date_day,
+day_of_week,
+month_of_year,
+month_name,
+quarter_of_year,
+year_number
 from cte_date
