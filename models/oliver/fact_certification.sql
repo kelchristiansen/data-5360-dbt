6e2e8091468fab63a40074afdc53142855c938d1
Chelsea Christiansen <chelsea.christiansen@usu.edu>
2025-10-26 20:36:34 +0000

    done for real

--- models/oliver/fact_certification.sql
+++ models/oliver/fact_certification.sql
@@ -10,7 +10,7 @@ select
     c.certification_cost
 from 
     {{ ref('stg_employee_certifications') }} c
-inner join {{ ref('dim_date') }} d
+left join {{ ref('dim_date') }} d
     on d.date_day = c.certification_awarded_date
-inner join {{ ref('dim_employee') }} e
+left join {{ ref('dim_employee') }} e
     on e.employee_id = c.employee_id
